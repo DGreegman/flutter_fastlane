3a37e8fd53f132245faa76f609a2c5fe457100f5
DGreegman <obeagug@gmail.com>
2025-09-27 13:57:27 +0100

    Initial Commit With the Material APP

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// stateless widget
+// material app
+// scaffold
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
