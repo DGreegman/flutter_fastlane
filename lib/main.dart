9df016b56dbf966118c89d37bc268ee213ae741d
DGreegman <obeagug@gmail.com>
2025-09-28 18:16:12 +0100

    Final for main

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,48 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Fastlane'),
+          centerTitle: true,
+          // leading: const Icon(Icons.login),
+          // actions: [
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          // ],
+          // backgroundColor: Colors.teal.shade700,
+        ),
+
+        drawer: Drawer(
+          child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: const Text('John Doe'),
+                accountEmail: const Text('john.doe@example.com'),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Hello WWorld");
+          },
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          selectedIndex: 2,
+        ),
+      ),
     );
   }
 }
